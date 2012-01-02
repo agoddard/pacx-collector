8eb01bb9dc3055752239fce25b6e2de566e07d47
Anthony Goddard <anthony@anthonygoddard.com>
2012-01-01 22:39:57 -0500

    basic version with triples output working

--- fetch.rb
+++ fetch.rb
@@ -2,16 +2,11 @@ require 'json'
 require 'net/http'
 require 'ap'
 
-# test Airmar PB200 weather station on glider Benjamin
-
-
-#If you don't specify any results variables, the results table will include columns for all of the variables in the dataset.
 
 def fetch_data(dataset)
   base_url = "http://data.liquidr.com/erddap/tabledap"
   timerange = '>=2011-12-26T00:00:00Z'
   url = "#{base_url}/#{dataset}.json?&time#{timerange}"
-  puts url
   resp = Net::HTTP.get_response(URI.parse(URI.encode(url)))
   data = resp.body
   result = JSON.parse(data)
@@ -21,6 +16,28 @@ def fetch_data(dataset)
   return result
 end
 
+def build_triples(dataset)
+  pacx = fetch_data(dataset)
+  attributes = pacx['table']['columnNames'].size - 1
+  columns = pacx['table']['columnNames']
+  raise "time column is wrong" if columns[2] != 'time'
+  pacx['table']['rows'].each_with_index do |record,index|
+    (0..attributes).each do |i|
+      puts "#{record[2]},#{dataset}.#{columns[i]},#{record[i]}"
+    end
+  end  
+end
 
-ap fetch_data('weatherBenjamin')
 
+sources = %w(weather CTDox DatawellMOSE PowerStatus basic Fluorometer)
+bots = %w(Benjamin FontaineMaru PapaMau PiccardMaru)
+
+#Hermes and MBARIOAWaveGlider data having issues currently
+
+
+sources.each do |source|
+  bots.each do |bot|
+    dataset = "#{source}#{bot}"
+    build_triples(dataset)
+  end
+end
