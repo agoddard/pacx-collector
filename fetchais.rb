293c611ffaeb341e1a2c9ca4bebbe6368aea3d61
Anthony Goddard <anthony@anthonygoddard.com>
2012-01-02 17:54:55 -0500

    Standalone AIS/MMSI query working

--- fetchais.rb
+++ fetchais.rb
@@ -1,4 +1,36 @@
 require_relative 'pacx.rb'
+require 'nokogiri'
+require 'open-uri'
 
 dataset = "ship"
-build_triples(dataset)
+# data = build_triples(dataset)
+
+base_url = "http://www.marinetraffic.com/ais/shipdetails.aspx?MMSI="
+
+#test id
+# id = 636091941 #cargo
+id = 244212000 #fishing
+
+page = Nokogiri::HTML(open("#{base_url}#{id}"))
+detail = page.at('//div[starts-with(@id,"detailtext")]')
+detail.children.each_with_index do |child,index|
+  case child.text
+  when 'Ship Type:'
+    @ship_type_index = index + 1
+  when 'Destination:'
+    @ship_destination_index = index +1
+  when 'Flag:'
+    @ship_flag_index = index + 1
+  when 'DeadWeight:'
+    @ship_deadweight_index = index + 1
+  when 'Speed recorded (Max / Average):'
+    @ship_speed_index = index + 1
+  end
+end
+ship_name = detail.xpath("//h1").first.text
+
+
+
+#calculate heading and distance from glider
+
+puts "We just passed the #{detail.children[@ship_type_index].to_s.split.first.downcase} vessel \"#{ship_name}\", she's a #{detail.children[@ship_flag_index].to_s.strip} flagged vessel on her way to #{detail.children[@ship_destination_index].to_s.strip.capitalize}"
