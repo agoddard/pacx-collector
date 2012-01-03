752e473149284ad4b2e6f70890829dc51890ab90
Anthony Goddard <anthony@anthonygoddard.com>
2012-01-02 23:51:57 -0500

    updated to work with new collections

--- fetchbots.rb
+++ fetchbots.rb
@@ -7,8 +7,10 @@ sources.each do |source|
   bots.each do |bot|
     dataset = "#{source}#{bot}"
     data = build_triples(dataset)
-    data.each do |triple|
-      puts "#{triple[:time].to_epoch},#{triple[:metric]},#{triple[:value]}"
+    data.each do |collection|
+      collection.each do |triple|
+        puts "#{triple[:time].to_epoch},#{triple[:metric]},#{triple[:value]}"
+      end
     end
   end
 end
