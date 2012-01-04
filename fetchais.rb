f594381ec4c7431b0e5c15e826d111f60dfe668c
Anthony Goddard <anthony@anthonygoddard.com>
2012-01-03 23:35:33 -0500

    now returning image, refactored ship_data, nicer formatting for tweet

--- fetchais.rb
+++ fetchais.rb
@@ -3,41 +3,80 @@ require 'nokogiri'
 require 'open-uri'
 
 dataset = "ship"
+
+
+def titleize_ship(stroing)
+  stroing.split.map{|w| w.capitalize}.join(' ').gsub('Ss','SS')
+end
+
+def ship_data(mssi_id)
+  base_url = "http://www.marinetraffic.com/ais/shipdetails.aspx?MMSI="
+  puts "#{base_url}#{mssi_id}"
+  page = Nokogiri::HTML(open("#{base_url}#{mssi_id}"))
+
+  raise "Ship not found" if page.at('//h2[contains(.,"Non-existent Vessel")]') || page.at('//h1[contains(.,"The requested service is unavailable.")]')
+
+  detail = page.at('//div[starts-with(@id,"detailtext")]')
+  ship = {}
+  detail.children.each_with_index do |child,index|
+    case child.text
+    when 'Ship Type:'
+      ship[:type] = extract_text(child.next_sibling).split.first.downcase
+    when 'Destination:'
+      ship[:destination] = extract_text(child.next_sibling).capitalize
+    when 'Flag:'
+      ship[:flag] = extract_text(child.next_sibling)
+    when 'DeadWeight:'
+      ship[:deadweight] = extract_text(child.next_sibling)
+    when 'Speed recorded (Max / Average):'
+      ship[:speed] = extract_text(child.next_sibling)
+    end
+  end
+  ship[:name] = titleize_ship(detail.xpath("//h1").first.text)
+  ship[:image] = page.css('img#picholder').first.attribute('src') rescue nil
+  
+  return ship
+end
+
+def extract_text(nokogiri_element)
+  nbsp = Nokogiri::HTML("&nbsp;").text
+  nokogiri_element.text.gsub(nbsp, " ").strip
+end
+
+def tweet(ship)
+  message = "We just passed the #{ship[:type]} vessel #{ship[:name]}, she's a #{ship[:flag]} flagged vessel"
+  message << " on her way to #{ship[:destination]}" unless ship[:destination].empty?
+  message
+end
+
+
+
 data = build_triples(dataset)
 
-base_url = "http://www.marinetraffic.com/ais/shipdetails.aspx?MMSI="
+# crank.filter.each
+
+dedup={}
 
 # find ships nearby
-puts data
-
-#test id
-# id = 636091941 #cargo
-id = 244212000 #fishing
-
-page = Nokogiri::HTML(open("#{base_url}#{id}"))
-detail = page.at('//div[starts-with(@id,"detailtext")]')
-detail.children.each_with_index do |child,index|
-  case child.text
-  when 'Ship Type:'
-    @ship_type_index = index + 1
-  when 'Destination:'
-    @ship_destination_index = index +1
-  when 'Flag:'
-    @ship_flag_index = index + 1
-  when 'DeadWeight:'
-    @ship_deadweight_index = index + 1
-  when 'Speed recorded (Max / Average):'
-    @ship_speed_index = index + 1
+data.each do |collection|
+  collection.each do |triple|
+    begin
+      puts tweet ship_data triple[:value] if triple[:metric] == "ship.MMSI" && !dedup[triple[:value]]
+      dedup[triple[:value]] = true
+    rescue RuntimeError
+    end
   end
 end
-ship_name = detail.xpath("//h1").first.text
+
+# puts tweet ship_data 636091941
+# puts tweet ship_data 244212000
+#test id
+# id = 636091941 #cargo
+# id = 244212000 #fishing
+# puts tweet ship_data 636091941
 
 
 
-#calculate heading and distance from glider
+# get first ship's sighting timestamp, ID, location, heading and speed
 
 
-
-
-
-puts "We just passed the #{detail.children[@ship_type_index].to_s.split.first.downcase} vessel \"#{ship_name}\", she's a #{detail.children[@ship_flag_index].to_s.strip} flagged vessel on her way to #{detail.children[@ship_destination_index].to_s.strip.capitalize}"
